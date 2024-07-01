206282de32ea31647930164af1735e43327b6b40
Abhishek Chauhan <abhishekchauhandz@gmail.com>
2024-07-01 15:21:46 +0530

    Fix ESLint issues with unescaped single quotes

--- src/app/screenshot/page.tsx
+++ src/app/screenshot/page.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import { useEffect } from 'react';
-import '../../../public/assets/vendor/swiper/swiper-bundle.min.css';
+import 'swiper/swiper-bundle.min.css';
 import '../../../public/assets/css/style.css';
 
 const Screenshots = () => {
@@ -45,46 +45,46 @@ const Screenshots = () => {
   }, []);
 
   return (
-
-    <section id="screenshots" className="padd-section text-center">
-      <div className="container" data-aos="fade-up">
-        <div className="section-title text-center">
-          <h2>App Gallery</h2>
-          <p className="separator">Integer cursus bibendum augue ac cursus .</p>
-        </div>
-
-        <div className="screens-slider swiper">
-          <div className="swiper-wrapper align-items-center">
-            <div className="swiper-slide">
-              <img src="/assets/img/screen/1.jpg" className="img-fluid" alt="" />
-            </div>
-            <div className="swiper-slide">
-              <img src="/assets/img/screen/2.jpg" className="img-fluid" alt="" />
-            </div>
-            <div className="swiper-slide">
-              <img src="/assets/img/screen/3.jpg" className="img-fluid" alt="" />
-            </div>
-            <div className="swiper-slide">
-              <img src="/assets/img/screen/4.jpg" className="img-fluid" alt="" />
-            </div>
-            <div className="swiper-slide">
-              <img src="/assets/img/screen/5.jpg" className="img-fluid" alt="" />
-            </div>
-            <div className="swiper-slide">
-              <img src="/assets/img/screen/6.jpg" className="img-fluid" alt="" />
-            </div>
-            <div className="swiper-slide">
-              <img src="/assets/img/screen/7.jpg" className="img-fluid" alt="" />
-            </div>
-            <div className="swiper-slide">
-              <img src="/assets/img/screen/8.jpg" className="img-fluid" alt="" />
-            </div>
+    <>
+      <section id="screenshots" className="padd-section text-center">
+        <div className="container" data-aos="fade-up">
+          <div className="section-title text-center">
+            <h2>App Gallery</h2>
+            <p className="separator">Integer cursus bibendum augue ac cursus.</p>
           </div>
-          <div className="swiper-pagination"></div>
-        </div>
-      </div>
-    </section>
 
+          <div className="screens-slider swiper">
+            <div className="swiper-wrapper align-items-center">
+              <div className="swiper-slide">
+                <img src="/assets/img/screen/1.jpg" className="img-fluid" alt="" />
+              </div>
+              <div className="swiper-slide">
+                <img src="/assets/img/screen/2.jpg" className="img-fluid" alt="" />
+              </div>
+              <div className="swiper-slide">
+                <img src="/assets/img/screen/3.jpg" className="img-fluid" alt="" />
+              </div>
+              <div className="swiper-slide">
+                <img src="/assets/img/screen/4.jpg" className="img-fluid" alt="" />
+              </div>
+              <div className="swiper-slide">
+                <img src="/assets/img/screen/5.jpg" className="img-fluid" alt="" />
+              </div>
+              <div className="swiper-slide">
+                <img src="/assets/img/screen/6.jpg" className="img-fluid" alt="" />
+              </div>
+              <div className="swiper-slide">
+                <img src="/assets/img/screen/7.jpg" className="img-fluid" alt="" />
+              </div>
+              <div className="swiper-slide">
+                <img src="/assets/img/screen/8.jpg" className="img-fluid" alt="" />
+              </div>
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
