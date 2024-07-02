9bafd0d7e0c078e8bf07cbc81d633b5bbaeb244b
Abhishek Chauhan <abhishekchauhandz@gmail.com>
2024-07-02 23:39:36 +0530

    added css to page and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect } from 'react';
 import aos from '../../public/assets/vendor/aos/aos'
+import "../../public/assets/css/style.css"
 import Link from 'next/link';
 
 import Header from './header/page';
