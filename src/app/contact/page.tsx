8e3615df51586d4675b71ae39955d5008e86158d
Abhishek Chauhan <abhishekchauhandz@gmail.com>
2024-07-03 15:38:30 +0530

    replaced path of firebaseconfig

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { db } from "../firebase/config";
+import { db } from "../../../firebaseConfig"
 import { collection, addDoc } from "firebase/firestore";
 import React, {useState} from "react"
 
